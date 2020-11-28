120a5e792dd1f67224b18e09be96ef677a7c0c93
anirrudh <anik597@gmail.com>
2020-11-27 21:26:23 -0500

    component init

---
+++ collaboredit/src/components/research.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const Research = (props: any) => {
+	return( <div>
+		<ul>
+			<li> An <a href="https://github.com/clintharris/crdt-example-app_annotated/blob/master/NOTES.md"> explanation </a> on how <a href="https://github.com/jlongster/crdt-example-app"> this</a> implementation works. </li>
+		</ul>
+			</div>
+	);
+}
+
+export default Research;
