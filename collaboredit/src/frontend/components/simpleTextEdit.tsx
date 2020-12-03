35c639d8cef68e040df448b814bc3384070c576f
anirrudh <anik597@gmail.com>
2020-12-03 15:22:39 -0500

    commit before refactor - some wiring needed

--- collaboredit/src/frontend/components/simpleTextEdit.tsx
+++ collaboredit/src/frontend/components/simpleTextEdit.tsx
@@ -1,36 +1,37 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
-import { test_automerge, init_am_doc, init_am_doc_text, capture_character_input } from "../../rtc/capture";
+import { test_automerge, init_am_doc, init_am_doc_text, capture_character_input, changes_am_doc, merge_am_doc, capture_character_delete } from "../../rtc/capture";
 import { Box, Button } from "rebass";
 import { Label, Textarea } from "@rebass/forms";
 
 const SimpleEditor = (props: any) => {
   const [text, setText] = useState(null);
-  const [textLen, setTextLen] = useState(0);
-  const [lastChar, setLastChar] = useState(null);
+  const [prevLen, setPrevLen] = useState(0);	       // The last length of an unmodified change
   const [currentDoc, setCurrentDoc] = useState({});            // Current Doc (refactor out)
-  const [changedDoc, setChangedDoc] = useState({}); 	  // Doc compiled with changes
+  const [changedDoc, setChangedDoc] = useState([]); 	       // Doc compiled with changes
 
   const handleText = (event: React.ChangeEvent) => {
     // Algo to check if charater was added or not
     let currLen = event.target.value.length;
-    let prevLen = textLen; 
-    console.log(prevLen, currLen);
     if (prevLen < currLen) {
-	setText(event.target.value); 
-	var text_char = event.target.value[currLen-1];
-	console.log(text_char);
-	setChangedDoc(capture_character_input(currentDoc, text_char, currLen));
+	let text_char = event.target.value[currLen-1];
+    	setChangedDoc(capture_character_input(currentDoc, changedDoc, text_char));
+    	setPrevLen(currLen);
+	test_automerge();
 	}
    else {
 	console.log("A character was deleted"); 
+	   //setChangedDoc(capture_character_delete(currentDoc, changedDoc, text_char));
+
     }
-    setTextLen(event.target.value.length);
-    () => { console.log(changedDoc) }
+
+    setText(event.target.value); 
     event.preventDefault();
   };
 
-  const broadcastChange = () => {}; 
+  const broadcastChanges = () => {
+    	changes_am_doc(currentDoc, changedDoc);
+	}; 
 
   /*
    * This function gets changes made on the
@@ -49,7 +50,8 @@ const SimpleEditor = (props: any) => {
       <Box p={5} fontSize={4}>
         <Label htmlFor="comment">Collaboration Text Area</Label>
         <Textarea id="comment" name="comment" onChange={handleText} />
-		<Button variant="primary" mr={2} onClick={handleGetChangesHost}> Check Host Changes </Button>
+	<br />
+	<Button variant="primary" mr={2} onClick={handleGetChangesHost}> Check Host Changes </Button>
       </Box>
     </div>
   );
--- collaboredit/src/rtc/capture.ts
+++ collaboredit/src/rtc/capture.ts
@@ -6,7 +6,7 @@ export function test_automerge() {
   // for changes to be staged and then computed later
   const newDoc = Automerge.change(currentDoc, "first message", (doc: any) => {
     doc.text = new Automerge.Text();
-    doc.text.insertAt(0, "h", "e", "l", "l", "o");
+    console.log(doc.text);
   });
   //const changes = Automerge.getChanges(currentDoc, newDoc);
 }
@@ -27,7 +27,16 @@ export const init_am_doc_text = () => {
 
 // Get the changes in state between the new document and the old one
 export const changes_am_doc = (oldDoc: any, newDoc: any) => {
+	console.log("Old", oldDoc);
+	console.log("New", newDoc);
 	return Automerge.getChanges(oldDoc, newDoc);
+}
+
+// Merge documents together and create one large document
+export const merge_am_doc = (oldDoc: any, newDoc: any) => {
+	console.log("Old", oldDoc);
+	console.log("New", newDoc);
+	return Automerge.merge(oldDoc, newDoc);
 }
 /*
  * This function takes in exactly one character
@@ -38,10 +47,23 @@ export const changes_am_doc = (oldDoc: any, newDoc: any) => {
  * Messages will be unique to the node that they are spawned from
  * and as a result will contain the ActorID as part of the signature. 
  */
-export function capture_character_input(currentDoc: any, text_char: any, insertion_index: number) {	
-//	var msg = Automerge.getActorId(currentDoc) + " - adding a new commit!";
-	return Automerge.change(currentDoc, 'suh', (doc: any) => {
-		doc.text = new Automerge.Text();
-		doc.text.insertAt(insertion_index  - 1, text_char);
+export const capture_character_input = (currentDoc: any, textDoc: any, text_char: any) => {	
+	var msg = Automerge.getActorId(currentDoc) + " - add char " + text_char;
+	return Automerge.change(currentDoc, msg, (doc: any) => {
+		doc.text = textDoc;
+		doc.text.insertAt(0, text_char);
 	});
+}
+
+/*
+ * This function handles a change of deletion
+ * of a character from the current form.
+ */
+export const capture_character_delete = (currentDoc: any, textDoc: any, text_char: any) => {
+       var msg = Automerge.getActorId(currentDoc) + " - deleted char " + text_char;
+       return Automerge.change(currentDoc, msg, (doc: any) => {
+	       doc.text = textDoc;
+	       console.log(doc.text); 
+	       doc.text.deleteAt(0, text_char);
+       });
 }
